22e7c919351c35459980925a20e565faa23ba409
cm360 <cm360@cm360.xyz>
2023-09-07 13:32:00 +0000

    i forgor to commit this file lol

---
+++ templates/js/utils.js
@@ -0,0 +1,49 @@
+// Code copied from W3Schools
+// https://www.w3schools.com/js/js_cookies.asp
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+const sizes = ['xs', 'sm', 'md', 'lg', 'xl'];
+
+function getViewportSize() {
+  // https://stackoverflow.com/a/8876069
+  const width = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0
+  )
+  if (width <= 576) return 'xs'
+  if (width <= 768) return 'sm'
+  if (width <= 992) return 'md'
+  if (width <= 1200) return 'lg'
+  return 'xl'
+}
+
+function compareViewportSizes(s1, s2) {
+  if (!sizes.includes(s1) || !sizes.includes(s2)) {
+    throw Error("invalid sizes for comparison");
+  }
+  return sizes.indexOf(s1) - sizes.indexOf(s2);
+}
+
+export { setCookie, getCookie, getViewportSize, compareViewportSizes };
