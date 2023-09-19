45a19d304f948157d95b9971e157e846f7d166a7
cm360 <31625347+cm-360@users.noreply.github.com>
2023-09-19 23:26:00 +0000

    more work on new frontend

--- templates/js/client.js
+++ templates/js/client.js
@@ -2,25 +2,38 @@ import { getCookie, setCookie } from "{{ url_for('serve_js', filename='utils.js'
 
 (() => {
 
+    // ########## Method Selection ##########
+
     // method selection form
     const methodForm = document.getElementById("methodForm");
-    const methodRadioFc = document.getElementById("methodRadioFc");
-    const methodRadioMii = document.getElementById("methodRadioMii");
-    const methodRadioFcLabel = document.getElementById("methodRadioFcLabel");
-    const methodRadioMiiLabel = document.getElementById("methodRadioMiiLabel");
+    // method info cards
+    const methodCardGroup = document.getElementById("methodCardGroup");
+    const methodCards = methodCardGroup.getElementsByClassName("card");
+    // wrapper buttons
+    const methodButtonFc = document.getElementById("methodButtonFc");
+    const methodButtonMii = document.getElementById("methodButtonMii");
 
-    function updateMethodRadio() {
-        methodRadioFcLabel.classList.remove("border-secondary-subtle", "border-primary");
-        methodRadioMiiLabel.classList.remove("border-secondary-subtle", "border-primary");
-        if (methodRadioFc.checked) {
-            methodRadioFcLabel.classList.add("border-primary");
-        } else if (methodRadioMii.checked) {
-            methodRadioMiiLabel.classList.add("border-primary");
+    function selectMethod() {
+
+    }
+
+    function updateMethodSelection(selectedCard, radioButton) {
+        for (let card of methodCards) {
+            if (selectedCard == card) {
+                card.classList.remove("border-secondary-subtle");
+                card.classList.add("border-primary");
+            } else {
+                card.classList.add("border-secondary-subtle");
+                card.classList.remove("border-primary");
+            }
         }
     }
-    
-    updateMethodRadio();
-    methodRadioFcLabel.addEventListener("click", updateMethodRadio);
-    methodRadioMiiLabel.addEventListener("click", updateMethodRadio);
+
+    for (let card of methodCards) {
+        const button = card.getElementByClassName("btn-check");
+        card.addEventListener("click", event => updateMethodSelection(card));
+    }
+
+    // ##########  ##########
 
 })();
