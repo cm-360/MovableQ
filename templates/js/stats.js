bbf07dcec90a60a448a3c5a6e437d4106cd86217
cm360 <31625347+cm-360@users.noreply.github.com>
2023-09-21 06:01:24 +0000

    convert to 4 spaces per indentation level

--- templates/js/stats.js
+++ templates/js/stats.js
@@ -1,25 +1,25 @@
 (() => {
 
-  const waitingCount = document.getElementById("waitingCount");
-  const workingCount = document.getElementById("workingCount");
-  const workerCount = document.getElementById("workerCount");
-  const movableTotal = document.getElementById("movableTotal");
+    const waitingCount = document.getElementById("waitingCount");
+    const workingCount = document.getElementById("workingCount");
+    const workerCount = document.getElementById("workerCount");
+    const movableTotal = document.getElementById("movableTotal");
 
-  async function getNetworkStats() {
-    const response = await fetch("{{ url_for('api_check_network_stats') }}");
-    if (response.ok) {
-      const responseJson = await response.json();
-      const stats = responseJson.data;
-      waitingCount.innerText = stats.waiting;
-      workingCount.innerText = stats.working;
-      workerCount.innerText = stats.workers;
-      movableTotal.innerText = stats.mseds_mined;
+    async function getNetworkStats() {
+        const response = await fetch("{{ url_for('api_check_network_stats') }}");
+        if (response.ok) {
+            const responseJson = await response.json();
+            const stats = responseJson.data;
+            waitingCount.innerText = stats.waiting;
+            workingCount.innerText = stats.working;
+            workerCount.innerText = stats.workers;
+            movableTotal.innerText = stats.mseds_mined;
+        }
     }
-  }
 
-  document.addEventListener('DOMContentLoaded', () => {
-    getNetworkStats();
-    setInterval(getNetworkStats, 15000);
-  });
+    document.addEventListener('DOMContentLoaded', () => {
+        getNetworkStats();
+        setInterval(getNetworkStats, 15000);
+    });
 
 })();
--- templates/js/utils.js
+++ templates/js/utils.js
@@ -2,59 +2,59 @@
 // https://www.w3schools.com/js/js_cookies.asp
 
 function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
-  let name = cname + "=";
-  let ca = document.cookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+    return "";
 }
 
 
 // Modified from https://stackoverflow.com/a/18650249
 
 function blobToBase64(blob) {
-  return new Promise((resolve, _) => {
-      const reader = new FileReader();
-      reader.onloadend = () => resolve(reader.result.split(',')[1]);
-      reader.readAsDataURL(blob);
-  });
+    return new Promise((resolve, _) => {
+        const reader = new FileReader();
+        reader.onloadend = () => resolve(reader.result.split(',')[1]);
+        reader.readAsDataURL(blob);
+    });
 }
 
 
 const sizes = ['xs', 'sm', 'md', 'lg', 'xl'];
 
 function getViewportSize() {
-  // https://stackoverflow.com/a/8876069
-  const width = Math.max(
-    document.documentElement.clientWidth,
-    window.innerWidth || 0
-  )
-  if (width <= 576) return 'xs'
-  if (width <= 768) return 'sm'
-  if (width <= 992) return 'md'
-  if (width <= 1200) return 'lg'
-  return 'xl'
+    // https://stackoverflow.com/a/8876069
+    const width = Math.max(
+        document.documentElement.clientWidth,
+        window.innerWidth || 0
+    )
+    if (width <= 576) return 'xs'
+    if (width <= 768) return 'sm'
+    if (width <= 992) return 'md'
+    if (width <= 1200) return 'lg'
+    return 'xl'
 }
 
 function compareViewportSizes(s1, s2) {
-  if (!sizes.includes(s1) || !sizes.includes(s2)) {
-    throw Error("invalid sizes for comparison");
-  }
-  return sizes.indexOf(s1) - sizes.indexOf(s2);
+    if (!sizes.includes(s1) || !sizes.includes(s2)) {
+        throw Error("invalid sizes for comparison");
+    }
+    return sizes.indexOf(s1) - sizes.indexOf(s2);
 }
 
 export { setCookie, getCookie, blobToBase64, getViewportSize, compareViewportSizes };
