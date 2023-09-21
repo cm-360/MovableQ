da02e7815643bd5cf57b953b32f44787ce1f93c7
cm360 <31625347+cm-360@users.noreply.github.com>
2023-09-21 03:40:15 +0000

    use + for job key separator

--- templates/js/client.js
+++ templates/js/client.js
@@ -354,7 +354,7 @@ import { getCookie, setCookie, blobToBase64 } from "{{ url_for('serve_js', filen
             const responseJson = await response.json();
             if (response.ok) {
                 // submission successful
-                const newJobKey = responseJson.data.join(",");
+                const newJobKey = responseJson.data.join("+");
                 setJobKey(newJobKey);
             } else {
                 // throw error with server message
