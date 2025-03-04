dfbb43e0159834800c128f7deada7e1538097576
CM360 <31625347+cm-360@users.noreply.github.com>
2025-03-04 04:15:38 -0500

    feat: Begin writing tests

---
+++ tests/test_jobs.py
@@ -0,0 +1,24 @@
+import pytest
+
+from . import app
+from . import client
+
+
+@pytest.mark.asyncio
+async def test_msed_job(client):
+    id0 = "969dbbb25e8f636c391ed29432e2af53"
+
+    # Create msed job
+    response = await client.post("/api/jobs", json={
+        "type": "msed",
+        "job": {
+            "id0": id0,
+        },
+    })
+    assert response.status_code == 200
+
+    data = await response.get_json()
+    assert data["id0"] == id0
+
+    response = await client.get("/api/jobs")
+    assert response.status_code == 200
