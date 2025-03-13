8a81c2a4ed3c1ba313c5a0dacfb1ed13efac9d27
CM360 <31625347+cm-360@users.noreply.github.com>
2025-03-13 02:48:56 -0400

    refactor: relocate v1 codebase

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -2,6 +2,7 @@ from quart import Quart
 from hypercorn.middleware import ProxyFixMiddleware
 
 from .api import bp as api_bp
+from .ui import bp as ui_bp
 from .db import db
 
 
@@ -10,6 +11,7 @@ def create_app() -> Quart:
     app.asgi_app = ProxyFixMiddleware(app.asgi_app, mode="legacy", trusted_hops=1)
 
     app.register_blueprint(api_bp, url_prefix="/api")
+    app.register_blueprint(ui_bp)
 
     db.init_app(app)
     db.create_all()
