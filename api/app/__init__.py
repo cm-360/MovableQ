9a435c82d9624b32ecb2896809848f93a7bfdbea
CM360 <31625347+cm-360@users.noreply.github.com>
2025-03-17 00:56:13 -0400

    fix: remove unused blueprint import

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -2,7 +2,6 @@ from quart import Quart
 from hypercorn.middleware import ProxyFixMiddleware
 
 from .api import bp as api_bp
-from .ui import bp as ui_bp
 from .db import db
 
 
@@ -11,7 +10,6 @@ def create_app() -> Quart:
     app.asgi_app = ProxyFixMiddleware(app.asgi_app, mode="legacy", trusted_hops=1)
 
     app.register_blueprint(api_bp, url_prefix="/api")
-    app.register_blueprint(ui_bp)
 
     db.init_app(app)
     db.create_all()
