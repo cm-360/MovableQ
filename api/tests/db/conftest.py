f922623c91afe590b6400c09e92f1c207598fd1f
CM360 <31625347+cm-360@users.noreply.github.com>
2025-03-20 17:37:17 -0400

    fix: No DB echo in tests

--- api/tests/db/conftest.py
+++ api/tests/db/conftest.py
@@ -5,7 +5,7 @@ from app.db import create_db
 
 @fixture
 def db():
-    db = create_db("sqlite:///:memory:", echo=True)
+    db = create_db("sqlite:///:memory:", echo=False)
     db.create_all()
 
     return db
