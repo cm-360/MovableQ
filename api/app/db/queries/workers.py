278507a6d84394a9a4e0a397078e9c22d477dd07
CM360 <31625347+cm-360@users.noreply.github.com>
2025-03-20 02:46:18 -0400

    feat: Worker update queries

--- api/app/db/queries/workers.py
+++ api/app/db/queries/workers.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+from datetime import timezone
+
 from sqlalchemy import select
+from sqlalchemy import update
 
 from app.db.models.workers import FriendbotWorker
 from app.db.models.workers import MinerWorker
 from app.db.models.workers import Worker
 from app.db.utils import from_dict
 from app.utils.validators import is_valid_friend_code
+
+allowed_update_columns = ["name", "last_ip", "version"]
 
 
 def create_or_update_worker(session, worker_type: str, worker_data: dict) -> Worker:
@@ -17,6 +23,7 @@ def create_or_update_worker(session, worker_type: str, worker_data: dict) -> Wor
             return create_miner_worker(session, worker_data)
 
         # Update existing worker
+        worker = update_miner_worker(session, client_id, worker_data)
     elif "friendbot" == worker_type:
         # Check for existing worker
         friend_code = worker_data["friend_code"]
@@ -26,6 +33,7 @@ def create_or_update_worker(session, worker_type: str, worker_data: dict) -> Wor
             return create_friendbot_worker(session, worker_data)
 
         # Update existing worker
+
     else:
         raise ValueError(f"Invalid worker type: {worker_type}")
 
@@ -46,6 +54,44 @@ def create_friendbot_worker(session, worker_data: dict) -> FriendbotWorker:
 
     session.add(worker)
     session.flush()
+
+    return worker
+
+
+def update_miner_worker(session, client_id: str, worker_data: dict) -> MinerWorker:
+    worker_data = {k: v for k, v in worker_data.items() if k in allowed_update_columns}
+    worker_data["updated_at"] = datetime.now(timezone.utc)
+
+    # Update worker in DB
+    worker = session.execute(
+        update(MinerWorker)
+        .where(MinerWorker.client_id == client_id)
+        .values(worker_data)
+    )
+    session.flush()
+
+    # Get updated worker object
+    worker = get_miner_worker(session, client_id)
+
+    return worker
+
+
+def update_friendbot_worker(
+    session, friend_code: str, worker_data: dict
+) -> FriendbotWorker:
+    worker_data = {k: v for k, v in worker_data.items() if k in allowed_update_columns}
+    worker_data["updated_at"] = datetime.now(timezone.utc)
+
+    # Update worker in DB
+    worker = session.execute(
+        update(FriendbotWorker)
+        .where(FriendbotWorker.friend_code == friend_code)
+        .values(worker_data)
+    )
+    session.flush()
+
+    # Get updated worker object
+    worker = get_friendbot_worker(session, friend_code)
 
     return worker
 
