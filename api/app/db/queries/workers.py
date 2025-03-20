dd96f54f19010dd8b931407b27e94418fef85c68
CM360 <31625347+cm-360@users.noreply.github.com>
2025-03-20 02:57:31 -0400

    doc: Worker queries

--- api/app/db/queries/workers.py
+++ api/app/db/queries/workers.py
@@ -14,6 +14,19 @@ allowed_update_columns = ["name", "last_ip", "version"]
 
 
 def create_or_update_worker(session, worker_type: str, worker_data: dict) -> Worker:
+    """Creates or updates a worker in the database.
+
+    Args:
+        session (Session): The database session object.
+        worker_type (str): The type of worker.
+        worker_data (dict): Dictionary of the worker's data.
+
+    Returns:
+        Worker: The newly-created or updated worker object.
+
+    Raises:
+        ValueError: If an invalid worker type is provided.
+    """
     if "miner" == worker_type:
         # Check for existing worker
         client_id = worker_data["client_id"]
@@ -41,6 +54,15 @@ def create_or_update_worker(session, worker_type: str, worker_data: dict) -> Wor
 
 
 def create_miner_worker(session, worker_data: dict) -> MinerWorker:
+    """Creates a new miner worker in the database.
+
+    Args:
+        session (Session): The database session object.
+        worker_data (dict): Dictionary of data for the new worker.
+
+    Returns:
+        MinerWorker: The newly-created miner worker object.
+    """
     worker = from_dict(MinerWorker, worker_data)
 
     session.add(worker)
@@ -50,6 +72,15 @@ def create_miner_worker(session, worker_data: dict) -> MinerWorker:
 
 
 def create_friendbot_worker(session, worker_data: dict) -> FriendbotWorker:
+    """Creates a new friendbot worker in the database.
+
+    Args:
+        session (Session): The database session object.
+        worker_data (dict): Dictionary of data for the new worker.
+
+    Returns:
+        FriendbotWorker: The newly-created friendbot worker object.
+    """
     worker = from_dict(FriendbotWorker, worker_data)
 
     session.add(worker)
@@ -59,6 +90,16 @@ def create_friendbot_worker(session, worker_data: dict) -> FriendbotWorker:
 
 
 def update_miner_worker(session, client_id: str, worker_data: dict) -> MinerWorker:
+    """Updates an existing miner worker in the database.
+
+    Args:
+        session (Session): The database session object.
+        client_id (str): The miner worker's unique client ID.
+        worker_data (dict): Dictionary of data to update the worker with.
+
+    Returns:
+        MinerWorker: The updated miner worker object.
+    """
     worker_data = {k: v for k, v in worker_data.items() if k in allowed_update_columns}
     worker_data["updated_at"] = datetime.now(timezone.utc)
 
@@ -79,6 +120,22 @@ def update_miner_worker(session, client_id: str, worker_data: dict) -> MinerWork
 def update_friendbot_worker(
     session, friend_code: str, worker_data: dict
 ) -> FriendbotWorker:
+    """Updates an existing friendbot worker in the database.
+
+    Args:
+        session (Session): The database session object.
+        friend_code (str): The friendbot worker's unique friend code.
+        worker_data (dict): Dictionary of data to update the worker with.
+
+    Returns:
+        MinerWorker: The updated friendbot worker object.
+
+    Raises:
+        ValueError: If the provided friend code is invalid.
+    """
+    if not is_valid_friend_code(friend_code):
+        raise ValueError("Invalid friend code")
+
     worker_data = {k: v for k, v in worker_data.items() if k in allowed_update_columns}
     worker_data["updated_at"] = datetime.now(timezone.utc)
 
@@ -97,6 +154,14 @@ def update_friendbot_worker(
 
 
 def get_all_workers(session):
+    """Retrieves all workers from the database.
+
+    Args:
+        session (Session): The database session object.
+
+    Returns:
+        list[Worker]: A list of all workers.
+    """
     workers = [
         *get_miner_workers(session),
         *get_friendbot_workers(session),
@@ -106,19 +171,58 @@ def get_all_workers(session):
 
 
 def get_miner_workers(session) -> list[MinerWorker]:
+    """Retrieves all miner workers from the database.
+
+    Args:
+        session (Session): The database session object.
+
+    Returns:
+        list[MinerWorker]: A list of miner worker objects.
+    """
     return session.scalars(select(MinerWorker)).all()
 
 
 def get_friendbot_workers(session) -> list[FriendbotWorker]:
+    """Retrieves all friendbot workers from the database.
+
+    Args:
+        session (Session): The database session object.
+
+    Returns:
+        list[FriendbotWorker]: A list of friendbot worker objects.
+    """
     return session.scalars(select(FriendbotWorker)).all()
 
 
 def get_miner_worker(session, client_id: str) -> MinerWorker | None:
+    """Retrieves a specific miner worker by their client ID.
+
+    Args:
+        session (Session): The database session object.
+        client_id (str): The miner worker's unique client ID.
+
+    Returns:
+        MinerWorker | None: The retrieved miner worker object, or None if not
+            found.
+    """
     statement = select(MinerWorker).filter(MinerWorker.client_id == client_id)
     return session.scalars(statement).first()
 
 
 def get_friendbot_worker(session, friend_code: str) -> FriendbotWorker | None:
+    """Retrieves a specific friendbot worker by their friend code.
+
+    Args:
+        session (Session): The database session object.
+        client_id (str): The friendbot worker's unique friend code.
+
+    Returns:
+        MinerWorker | None: The retrieved friendbot worker object, or None if
+            not found.
+
+    Raises:
+        ValueError: If the provided friend code is invalid.
+    """
     if not is_valid_friend_code(friend_code):
         raise ValueError("Invalid friend code")
 
